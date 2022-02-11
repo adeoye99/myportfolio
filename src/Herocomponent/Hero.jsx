185e592c8eeb95812c26fe647b2a71b086719ea3
adeoyesulaimon@yahoo.com <adeoyesulaimon@yahoo.com>
2022-02-11 17:37:25 +0100

    responsive

--- src/Herocomponent/Hero.jsx
+++ src/Herocomponent/Hero.jsx
@@ -26,42 +26,7 @@ overflow:hidden;
 position:relative;
 `
 
-const HeroSlider= styled.div`
-position: absolute;
-top:0;
-left:0;
-width:100%;
-height:100%;
-display:flex;
-align-items:center;
-justify-content:center;
 
-&:before{
-    content: '';
-    position: absolute;
-    z-index:2;
-    width:100%;
-    height:100vh;
-    bottom: 0vh;
-    left:0;
-    overflow:hidden;
-    opacity : 0.4;
-    background: linear-gradient(
-        0deg,
-        rgba(0,0,0,0.2) 0%,
-        rgba(0,0,0,0.2) 50%,
-        rgba(0,0,0,0.6) 100%,
-    )
-}
-`
-const HeroImage = styled.img`
-position:absolute;
-top:0;
-left:0;
-width:100vw;
-height:100vh;
-object-fit: cover;
-`
 
 const HeroContent = styled.div`
 position:absolute;
