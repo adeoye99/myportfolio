3161d89dd0d170cc428b50bdff1f0817b8118708
adeoyesulaimon@yahoo.com <adeoyesulaimon@yahoo.com>
2022-02-21 01:53:09 +0100

    contact linking

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -11,6 +11,7 @@ const Contactcontainer = styled.div`
  flex-direction:column;
  margin-left:auto;
  margin-right:auto;
+ margin-bottom:30px;
 `
 const Contactform = styled.form`
  display:flex;
@@ -20,32 +21,43 @@ const Contactform = styled.form`
 `
 const Contactinput = styled.input`
  width:500px;
+ height:30px;
+ margin-top:50px;
+`
+const Contactinputmessage = styled.input`
+ width:500px;
+ height:100px;
  margin-top:50px;
 `
 const Contactbutton = styled.button`
- width:500px;
+ width:100px;
+ justify-content:center;
+ text-align:center;
  margin-top:50px;
 `
-
+const API_ID = process.env.REACT_APP_CLIENT_ID;
+console.log(process.env.REACT_APP_CLIENT_ID);
 function Contact() {
   function sendEmail(e){
-    console.log(process.env.REACT_APP_CLIENT_ID);
-const API_KEY = process.env.REACT_APP_CLIENT_ID;
     e.preventDefault();
    
-    emailjs.sendForm('gmail', 'template_rgjtao7', e.target, API_KEY)
+    emailjs.sendForm('gmail' , 'template_rgjtao7', e.target,`${API_ID}`)
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+      e.target.reset();
   }
   return (
     <Contactcontainer>
         <Contactform onSubmit={sendEmail}>
-          <Contactinput type= "text" placeholder="Name" />
-          <Contactinput type= "text" placeholder = "Message" />
-          <Contactbutton>Send</Contactbutton>
+          <Contacttitle>Contact Me</Contacttitle>
+          <Contactinput type= "text" placeholder="Name" name = "name" />
+          <Contactinput type= "email" placeholder="Email" name = "email"/>
+          <Contactinput type= "text" placeholder="Subject" name = "subject"/>
+          <Contactinputmessage type= "text" placeholder = "Message" name = "message" />
+          <Contactbutton type = "submit" value = "message">Send</Contactbutton>
         </Contactform>
     </Contactcontainer>
   )
